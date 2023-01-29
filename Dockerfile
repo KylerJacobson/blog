f606efb1038dc3cf4044916e03b6a6a9bf34fc0c
Kyler Jacobson <code.63cl4@simplelogin.com>
2023-01-29 14:51:00 -0700

    Creates basic Dockerifle and homepage

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM amazoncorretto:17-alpine-full
+RUN addgroup -S spring && adduser -S spring -G spring
+USER spring:spring
+ARG JAR_FILE=target/blog-0.0.1-SNAPSHOT.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/dev/kylerjacobson/blog/BlogApplication.java
+++ src/main/java/dev/kylerjacobson/blog/BlogApplication.java
@@ -2,9 +2,17 @@ package dev.kylerjacobson.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BlogApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "My Blog";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
