2feddd5af312ac51ae5452172ae98fe0df27ca9b
Kyler Jacobson <github.beuog@simplelogin.com>
2023-08-09 19:50:55 -0600

    Serve the React app via Express

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    res.send({ message: "Hello from Github Actions!" });
+app.use(express.static(path.join(__dirname, "public/")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/", "index.html"));
 });
 
 app.listen(port, () => {
