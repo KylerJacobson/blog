e67616ec1451305bd52bbb69184cd8eddc154ae5
Kyler Jacobson <kylerjacobson@pm.me>
2023-08-14 17:24:24 -0600

    Adds PostgreSQL database connectivity

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { Pool } = require("pg");
+require("dotenv").config({ path: "../.env" });
 
 const port = process.env.PORT || 8080;
+
+const pool = new Pool({
+    connectionString: process.env.DBConnLink,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+app.get("/api/test", async (req, res) => {
+    try {
+        const allItems = await pool.query("SELECT * FROM test");
+        res.json({ allItems });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+});
 
 app.use(express.static(path.join(__dirname, "public/")));
 
@@ -11,5 +30,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
