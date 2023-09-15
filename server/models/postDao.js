3e5c4258a0caa02559ef83e37cf9e66bbfd6f2d9
Kyler Jacobson <kylerjacobson@pm.me>
2023-09-14 21:58:26 -0600

    try path

--- server/models/postDao.js
+++ server/models/postDao.js
@@ -1,7 +1,9 @@
 const { Pool } = require("pg");
 const dotenv = require("dotenv");
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+require("dotenv").config({
+    path: path.resolve(__dirname, "../../etc/secrets/.env"),
+});
 
 const pool = new Pool({
     connectionString: process.env.DBConnLink,
--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ const session = require("express-session");
 const UserDao = require("../server/models/userDao");
 const PostDao = require("../server/models/postDao");
 const dotenv = require("dotenv");
-require("dotenv").config({ path: path.resolve(__dirname, ". ./.env") });
+require("dotenv").config({
+    path: path.resolve(__dirname, "../etc/secrets/.env"),
+});
 
 const JWT_SECRET = process.env.JWTSecret;
 const SESSION_SECRET = process.env.SessionSecret;
