50cb012fb25cee1ddff9b2929016af4e968e6aa7
Kyler Jacobson <kylerjacobson@pm.me>
2024-01-10 07:41:08 -0700

    Updates code to not send email for local developemnet and fixes account update bug

--- server/services/notification.service.js
+++ server/services/notification.service.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
+const { ROLE } = require("../constants/roleConstants");
 const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const environment = process.env.ENVIRONMENT;
 
 class NotificationService {
     async notify(user, post) {
-        if (post.restricted && (user.role === 0 || user.role === -1)) {
-            console.log(
-                `${user.first_name} does not have permissions for post`
-            );
+        if (
+            post.restricted &&
+            (user.role === ROLE.NON_PRIVILEGED || user.role === ROLE.REQUESTED)
+        ) {
             return;
         }
         const msg = {
@@ -18,10 +20,11 @@ class NotificationService {
             text: "There is a new post on kylerjacobson.dev",
             html: `Hey ${user.first_name}, there is a new post on kylerjacobson.dev. Check out <a href="www.kylerjacobson.dev/signin">${post.title}</a>`,
         };
-        console.log(msg);
-        sgMail.send(msg).catch((error) => {
-            console.error(error);
-        });
+        if (environment !== "dev") {
+            sgMail.send(msg).catch((error) => {
+                console.error(error);
+            });
+        }
         return;
     }
 }
