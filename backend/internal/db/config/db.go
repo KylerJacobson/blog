a13a3b6d894f48e3ad3527341f93053591872af5
Kyler Jacobson <40446054+KylerJacobson@users.noreply.github.com>
2025-02-25 14:51:28 -0700

    updates db config to use explicit values

--- backend/internal/db/config/db.go
+++ backend/internal/db/config/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/KylerJacobson/blog/backend/logger"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -19,10 +20,28 @@ func GetDBConn(logger logger.Logger) *pgxpool.Pool {
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, db)
 
 	logger.Sugar().Infof("Trying to connect to database %s", connStr)
-	pool, err := pgxpool.New(context.Background(), connStr)
+
+	config, err := pgxpool.ParseConfig(connStr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		logger.Sugar().Errorf("Unable to parse connection string: %v", err)
 		os.Exit(1)
 	}
+	config.MaxConns = 10
+	config.MinConns = 2
+	config.MaxConnLifetime = 1 * time.Hour
+	config.MaxConnIdleTime = 30 * time.Minute
+	config.HealthCheckPeriod = 1 * time.Minute
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		logger.Sugar().Errorf("Unable to connect to database: %v", err)
+		os.Exit(1)
+	}
+
+	if err := pool.Ping(context.Background()); err != nil {
+		logger.Sugar().Errorf("Unable to ping database: %v", err)
+		os.Exit(1)
+	}
+
 	return pool
 }
